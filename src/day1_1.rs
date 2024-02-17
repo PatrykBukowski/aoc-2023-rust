adce996d89c60abf8de6c0708c4750f9b2b30d7d
PatrykBukowski <patrykbukowski96@gmail.com>
2024-02-17 13:25:22 +0100

    Extract 'read_content' functionality

--- src/day1_1.rs
+++ src/day1_1.rs
@@ -1,36 +1,41 @@
 use std::{fs::File, io::Read};
 
 pub fn run() {
-    let mut file = File::open("src/day1_2.txt").expect("File not found");
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents).unwrap();
-
-    let content_vector = contents.split("\n");
+    let contents = read_content("src/day1_2.txt");
 
     let mut new_vec: Vec<u32> = Vec::new();
 
-    for element in content_vector {
-        let mut preresult = String::new();
+    for element in contents {
+        if element.len() == 0 {
+            continue;
+        }
+        let mut result = String::new();
 
         for current_char in element.chars() {
             if current_char.is_digit(10) {
-                preresult.push(current_char);
+                result.push(current_char);
             }
         }
-        let a = preresult.clone();
-        let b = preresult.clone();
 
-        if preresult.len() > 0 {
-            new_vec.push(
-                (a.chars().next().unwrap().to_string() + &b.chars().last().unwrap().to_string())
-                    .parse()
-                    .unwrap(),
-            );
-        }
+        let result = result.to_owned().chars().next().unwrap().to_string()
+            + &result.chars().last().unwrap().to_string();
+
+        new_vec.push(result.parse().unwrap_or(0));
     }
-
     let res: u32 = new_vec.iter().sum();
 
     println!("{res}");
+}
+
+fn read_content(filename: &str) -> Vec<String> {
+    let mut file = File::open(filename).expect("File not found");
+    let mut contents = String::new();
+    let mut result: Vec<String> = Vec::new();
+
+    file.read_to_string(&mut contents).unwrap_or(0);
+
+    for res in contents.split("\n") {
+        result.push(res.to_string());
+    }
+    result
 }
